827002c370632f0e0057ab60b74f49cfbd627ca3
x <vtikhonov>
2020-01-30 15:36:45 +0300

    vuetify add theme

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -4,4 +4,22 @@ import Vuetify from 'vuetify/lib';
 Vue.use(Vuetify);
 
 export default new Vuetify({
+  theme: {
+    themes: {
+      light: {
+        primary: '#29b6f6',
+        primaryDark: '#0086c3',
+        primaryLight: '#73e8ff',
+        secondary: '#80d8ff',
+        secondaryDark: '#49a7cc',
+        secondaryLight: '#b5ffff',
+        primaryText: '#fff',
+        secondaryText: '#000',
+        error: '#e53935',
+        warning: '#ffeb3b',
+        success: '#0086c3',
+        accent: '#F6FFFA',
+      },
+    }
+  }
 });
