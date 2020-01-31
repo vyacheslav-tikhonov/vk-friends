1ed3f3214eeb8d1c7930559e37a4b8ae46f20213
x <vtikhonov>
2020-01-31 11:54:30 +0300

    add app buttons

---
+++ src/components/buttons/LoginButton/LoginButton.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <v-btn
+      color="primary"
+      @click="onButtonClick()">
+      Войти с помощью VK
+    </v-btn>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Inject} from 'vue-property-decorator';
+
+@Component({})
+export default class LoginButton extends Vue {
+  @Inject('onLogin') private login!: any;
+
+  private onButtonClick() {
+    this.login();
+  }
+}
+</script>
+
+<style scoped>
+</style>
---
+++ src/components/buttons/LogoutButton/LogoutButton.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <v-btn
+      block
+      text
+      @click="onButtonClick()">
+      Выход
+    </v-btn>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Inject } from 'vue-property-decorator';
+
+@Component({})
+export default class LogOutButton extends Vue {
+  @Inject('onLogout') private logout!: any;
+
+  private onButtonClick() {
+    this.logout();
+  }
+}
+</script>
+
+<style scoped>
+</style>
