e42eee090d34d9f411230160b5159818698b6fee
x <vtikhonov>
2020-01-31 12:01:40 +0300

    add user icon component

---
+++ src/components/user/UserIcon/UserIcon.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <v-skeleton-loader
+      :loading="!photoUrl"
+      rounded
+      transition="fade-transition"
+      height="50px"
+      type="avatar"
+    >
+      <v-avatar>
+        <v-img
+          class="v-avatar"
+          rounded
+          :src="photoUrl"/>
+      </v-avatar>
+    </v-skeleton-loader>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from 'vue-property-decorator';
+
+@Component({})
+export default class UserIcon extends Vue {
+  @Prop(String) private readonly photoUrl!: string;
+}
+</script>
+
+<style scoped>
+</style>
